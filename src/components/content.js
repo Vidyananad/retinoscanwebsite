9a10c9176234a947294a81d920aecee9fe348165
Vidyananad <paakavidyanandsai@gmail.com>
2024-03-13 15:32:35 +0530

    third commit

--- src/components/content.js
+++ src/components/content.js
@@ -82,10 +82,14 @@ const Content = () => {
           </div>
       </div>
      </div> 
-     <div>
+     <div className="upload-button-cont">
         <center>
-            <button className="button-item-new">Upload Fundus Image</button>
-            <button className="button-item-new">Upload OCT Image</button>
+            <button className="button-item-new"><div className="button-content"><p className="upload-name">Upload Fundus Image </p><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
+            </svg></div></button>
+            <button className="button-item-new"><div className="button-content"><p className="upload-name">Upload OCT Image</p><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
+            </svg></div></button>
         </center>
        </div>
     </>
